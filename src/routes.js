ac887b6fb42f8d155f74a6f76813b394daa6ac4a
Felipe Seolin Bento <bentof@alunos.utfpr.edu.br>
2019-09-25 15:42:28 -0300

    New routes

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,64 @@
 import React from 'react';
+
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-
+// Screens
 import HomeScreen from './screens/HomeScreen/HomeScreen';
-import ConfigScreen from './screens/ConfigScreen/ConfigScreen';
 import MyTripsScreen from './screens/MyTripsScreen/MyTripsScreen';
+import ProfileScreen from './screens/ProfileScreen/ProfileScreen';
+import SavedScreen from './screens/SavedScreen/SavedScreen';
+import SearchScreen from './screens/SearchScreen/SearchScreen';
+// Icons
+// import Teste from '../assets/home.svg';
 
-const homeStack = createBottomTabNavigator(
-    {
-        HomeScreen,
-        ConfigScreen,
-        MyTripsScreen
+const tabNavigator = createBottomTabNavigator({
+    HomeScreen: {
+        screen: HomeScreen,
+        navigationOptions: {
+            tabBarLabel: 'Início'
+        }
+    },
+    SearchScreen: {
+        screen: SearchScreen,
+        navigationOptions: {
+            tabBarLabel: 'Buscar'
+        }
+    },
+    MyTripsScreen: {
+        screen: MyTripsScreen,
+        navigationOptions: {
+            tabBarLabel: 'Viagens'
+        }
+    },
+    SavedScreen: {
+        screen: SavedScreen,
+        navigationOptions: {
+            tabBarLabel: 'Salvos'
+        }
+    },
+    ProfileScreen: {
+        screen: ProfileScreen,
+        navigationOptions: {
+            tabBarLabel: 'Usuário'
+        }
     }
-);
+});
 
-const startStack = createStackNavigator(
+const stackNavigator = createStackNavigator(
     {
         HomeScreen
     },
     {
-        defaultNavigationOptions: { header: null }
+        defaultNavigationOptions: {
+            header: null
+        }
     }
 );
 
-const AppNavigator = createSwitchNavigator(
-    {
-        homeStack
-    }
-);
+const AppNavigator = createSwitchNavigator({
+    tabNavigator,
+    stackNavigator
+});
 
 export default Routes = createAppContainer(AppNavigator);
